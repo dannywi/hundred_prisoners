4d06f720afb5d716dc209645d94b27ee253b63e6
Danny Widjaya <dannywidjaya@DannynoiMac.local>
2022-07-02 21:01:54 +0900

    Add readme and clean up variable names

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# HELP THE PRISONERS SURVIVE
+# Help The Prisoners Survive!
 
-Saw this youtube math problem, and wanted to check the solution by monte-carlo.
+Saw this math problem on youtube, and wanted to check the solution by monte-carlo.
 
 https://www.youtube.com/watch?v=iSNsgj1OCLA
 
@@ -12,10 +12,19 @@ In summary:
 - If all 100 prisoners find their number during their turn in the room, they will all be freed. But if even one fails, they will all be executed
 - What is their best strategy to survive?
 
+As you'd imagine, with each prisoner picking the boxes at random the probability of survival would be 0.5 * 0.5 * 0.5 * ... = 0.5 to the power of 100, practically impossible.
+
 # Running the simulator
+This runs all the picker strategies and prints the result
 ```bash
 ./compile_run.sh
 ```
 
+Change these params to play with different numbers of trials and prisoners
+```bash
+  constexpr size_t PRISONERS = 14;
+  constexpr size_t TRIALS = 100'000;
+```
+
 # Conclusion
-What do you know, it actually works! Borderline witchcraft.
+What do you know, it actually works! Borderline witchcraft if you ask me.
--- main.cc
+++ main.cc
@@ -12,23 +12,23 @@ using namespace std;
 int main() {
   cout << "Help the prisoners survive!" << endl;
 
-  constexpr size_t PRISONER_COUNT = 12;
+  constexpr size_t PRISONERS = 14;
   constexpr size_t TRIALS = 100'000;
 
   struct Combo {
     const char* title;
-    bool(*picker)(const hundred::Boxes<PRISONER_COUNT>&, const hundred::Prisoner&);
+    bool(*picker)(const hundred::Boxes<PRISONERS>&, const hundred::Prisoner&);
     size_t survived_count;
   };
 
-  std::vector<Combo> combos {
+  vector<Combo> combos {
     { "picker_random", hundred::picker_random, 0 },
     { "picker_follow", hundred::picker_follow, 0 },
   };
 
   for (size_t i = 0; i < TRIALS; ++i) {
-    hundred::Boxes<PRISONER_COUNT> boxes;
-    auto prisoners = hundred::Prisoner::generate_prisoners<PRISONER_COUNT>();
+    hundred::Boxes<PRISONERS> boxes;
+    auto prisoners = hundred::Prisoner::generate_prisoners<PRISONERS>();
 
     for (auto& combo : combos) {
       #ifdef LOG_DEBUG
@@ -49,7 +49,7 @@ int main() {
 
   cout << "===========" << endl;
   cout << "TRIALS: " << TRIALS << endl;
-  cout << "PRISONERS: " << PRISONER_COUNT << endl;
+  cout << "PRISONERS: " << PRISONERS << endl;
   for (auto const& combo : combos) {
     cout << "  " << combo.title << endl;
     cout << "    SURVIVED: " << combo.survived_count
